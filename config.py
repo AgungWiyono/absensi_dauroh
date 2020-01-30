e87112b3179e97ff4987c71389f4b76aeceb44a7
Agung Wiyono <wiyonoagung1@gmail.com>
2020-01-30 09:08:00 +0700

    Initial API

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+import os
 from configparser import ConfigParser
 
 parser = ConfigParser()
 parser.read("config.ini")
 
+ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
 CARD_MAKER_CONFIG = dict(parser.items("CARDMAKER_CONF"))
 SPREADSHEET_CONF = dict(parser.items("EXCEL_CONF"))
