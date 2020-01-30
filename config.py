35aa56c38da2cfd8a0578b4147f0614cc9f5c7f0
Agung Wiyono <wiyonoagung1@gmail.com>
2020-01-30 10:00:43 +0700

    Fix config reader

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import os
 from configparser import ConfigParser
 
 parser = ConfigParser()
-parser.read("config.ini")
 
 ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
+parser.read(ROOT_PATH + "/config.ini")
+
 CARD_MAKER_CONFIG = dict(parser.items("CARDMAKER_CONF"))
 SPREADSHEET_CONF = dict(parser.items("EXCEL_CONF"))
